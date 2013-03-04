ab2383ac16b03e07c99615d1acd128bc658c7001
Isaac Petersen <dadrivr@gmail.com>
2013-03-03 22:38:53 -0500

    update source

--- R Scripts/Source.R
+++ R Scripts/Source.R
@@ -12,5 +12,13 @@ source(paste(getwd(),"/R Scripts/Calculate League Projections.R", sep=""), echo=
 #4. Evaluate league projections from last year
 source(paste(getwd(),"/R Scripts/Evaluate Projections.R", sep=""), echo=TRUE)
 
-#5. Calculate players' risk levels
+#5. Calculate players' risk levels (http://www.drewconway.com/zia/?p=2305)
 source(paste(getwd(),"/R Scripts/Risk.R", sep=""), echo=TRUE)
+
+#6. Calculate VOR for snake draft (https://harvardsportsanalysis.wordpress.com/2012/04/13/optimizing-draft-strategies-in-fantasy-football-abstract/)
+
+#7. Download average auction values
+
+#8. Calculate optimal team (https://office.microsoft.com/en-us/excel-help/pick-your-fantasy-football-team-with-solver-HA001124603.aspx)
+
+#9. Calculate Bid Up To
