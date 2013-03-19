0947ca3bea96552f2ccab40bf6fb29da2c479f2f
Isaac Petersen <dadrivr@gmail.com>
2013-03-18 22:53:49 -0400

    20130318

---
+++ R Scripts/Understanding Risk.R
@@ -0,0 +1,11 @@
+playerA <- rnorm(1000000, mean=150, sd=5)
+playerB <- rnorm(1000000, mean=150, sd=15)
+playerC <- rnorm(1000000, mean=150, sd=30)
+
+mydata <- data.frame(playerA, playerB, playerC)
+
+points <- c(mydata$playerA,mydata$playerB,mydata$playerC)
+player <- c(rep("Player A",dim(mydata)[1]),rep("Player B",dim(mydata)[1]),rep("Player C",dim(mydata)[1])) #,rep("Latent",dim(projections)[1])
+mydata2 <- data.frame(points,player)
+
+ggplot(mydata2, aes(x=points, fill=player)) + geom_density(alpha=.3) + xlab("Players' Projected Points") + ggtitle("Density Plot of Projected Points for 3 Players") + theme(legend.title=element_blank())
