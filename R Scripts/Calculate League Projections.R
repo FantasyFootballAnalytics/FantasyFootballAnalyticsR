ac9c8a536f3a9a65fd4f80dd3fc8f6a502d1c5dc
Isaac Petersen <isaactpetersen@gmail.com>
2014-06-04 09:53:34 -0400

    20140604 - added median projections

--- R Scripts/Calculate League Projections.R
+++ R Scripts/Calculate League Projections.R
@@ -69,6 +69,23 @@ projections$recTds <- rowMeans(projections[,paste("recTds", sourcesOfProjections
 projections$twoPts <- rowMeans(projections[,paste("twoPts", sourcesOfProjectionsAbbreviation, sep="_")], na.rm=TRUE)
 projections$fumbles <- rowMeans(projections[,paste("fumbles", sourcesOfProjectionsAbbreviation, sep="_")], na.rm=TRUE)
 
+#Calculate Hodges-Lehmann (pseudo-median) average of categories
+pb <- txtProgressBar(min = 0, max = dim(projections)[1], style = 3)
+for(i in 1:dim(projections)[1]){
+  setTxtProgressBar(pb, i)
+  
+  projections$passYdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("passYds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("passYds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$passTdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("passTds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("passTds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$passIntMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("passInt", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("passInt", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$rushYdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("rushYds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("rushYds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$rushTdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("rushTds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("rushTds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$recMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("rec", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("rec", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$recYdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("recYds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("recYds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$recTdsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("recTds", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("recTds", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$twoPtsMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("twoPts", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("twoPts", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+  projections$fumblesMedian[i] <- tryCatch(wilcox.test(as.numeric(as.vector(projections[i,paste("fumbles", sourcesOfProjectionsAbbreviation, sep="_")])), conf.int=TRUE, na.action="na.exclude")$estimate, error=function(e) median(as.numeric(as.vector(projections[i,paste("fumbles", sourcesOfProjectionsAbbreviation, sep="_")])), na.rm=TRUE))
+}
+
 #Convert NA to 0
 #projections[is.na(projections$passYds)==TRUE,"passYds"] <- 0
 #projections[is.na(projections$passTds)==TRUE,"passTds"] <- 0
@@ -94,16 +111,16 @@ projections$fumbles <- rowMeans(projections[,paste("fumbles", sourcesOfProjectio
 #}
 
 #Check projections
-projections[,c("name",paste("passYds", sourcesOfProjectionsAbbreviation, sep="_"),"passYds")]
-projections[,c("name",paste("passTds", sourcesOfProjectionsAbbreviation, sep="_"),"passTds")]
-projections[,c("name",paste("passInt", sourcesOfProjectionsAbbreviation, sep="_"),"passInt")]
-projections[,c("name",paste("rushYds", sourcesOfProjectionsAbbreviation, sep="_"),"rushYds")]
-projections[,c("name",paste("rushTds", sourcesOfProjectionsAbbreviation, sep="_"),"rushTds")]
-projections[,c("name",paste("rec", sourcesOfProjectionsAbbreviation, sep="_"),"rec")]
-projections[,c("name",paste("recYds", sourcesOfProjectionsAbbreviation, sep="_"),"recYds")]
-projections[,c("name",paste("recTds", sourcesOfProjectionsAbbreviation, sep="_"),"recTds")]
-projections[,c("name",paste("twoPts", sourcesOfProjectionsAbbreviation, sep="_"),"twoPts")]
-projections[,c("name",paste("fumbles", sourcesOfProjectionsAbbreviation, sep="_"),"fumbles")]
+projections[,c("name",paste("passYds", sourcesOfProjectionsAbbreviation, sep="_"), c("passYds","passYdsMedian"))]
+projections[,c("name",paste("passTds", sourcesOfProjectionsAbbreviation, sep="_"), c("passTds","passTdsMedian"))]
+projections[,c("name",paste("passInt", sourcesOfProjectionsAbbreviation, sep="_"), c("passInt","passIntMedian"))]
+projections[,c("name",paste("rushYds", sourcesOfProjectionsAbbreviation, sep="_"), c("rushYds","rushYdsMedian"))]
+projections[,c("name",paste("rushTds", sourcesOfProjectionsAbbreviation, sep="_"), c("rushTds","rushTdsMedian"))]
+projections[,c("name",paste("rec", sourcesOfProjectionsAbbreviation, sep="_"), c("rec","recMedian"))]
+projections[,c("name",paste("recYds", sourcesOfProjectionsAbbreviation, sep="_"), c("recYds","recYdsMedian"))]
+projections[,c("name",paste("recTds", sourcesOfProjectionsAbbreviation, sep="_"), c("recTds","recTdsMedian"))]
+projections[,c("name",paste("twoPts", sourcesOfProjectionsAbbreviation, sep="_"), c("twoPts","twoPtsMedian"))]
+projections[,c("name",paste("fumbles", sourcesOfProjectionsAbbreviation, sep="_"), c("fumbles","fumblesMedian"))]
 
 #Calculate projected points for your league (average projections)
 projections$passYdsPts <- projections$passYds * passYdsMultiplier
@@ -114,12 +131,25 @@ projections$rushTdsPts <- projections$rushTds * rushTdsMultiplier
 projections$recPts <- projections$rec * recMultiplier
 projections$recYdsPts <- projections$recYds * recYdsMultiplier
 projections$recTdsPts <- projections$recTds * recTdsMultiplier
+projections$twoPtsPts <- projections$twoPts * twoPtsMultiplier
 projections$fumblesPts <- projections$fumbles * fumlMultiplier
 
-projections$projectedPtsAvg <- rowSums(projections[,c("passYdsPts","passTdsPts","passIntPts","rushYdsPts","rushTdsPts","recPts","recYdsPts","recTdsPts","twoPts","fumblesPts")], na.rm=T)
+projections$passYdsMedianPts <- projections$passYdsMedian * passYdsMultiplier
+projections$passTdsMedianPts <- projections$passTdsMedian * passTdsMultiplier
+projections$passIntMedianPts <- projections$passIntMedian * passIntMultiplier
+projections$rushYdsMedianPts <- projections$rushYdsMedian * rushYdsMultiplier
+projections$rushTdsMedianPts <- projections$rushTdsMedian * rushTdsMultiplier
+projections$recMedianPts <- projections$recMedian * recMultiplier
+projections$recYdsMedianPts <- projections$recYdsMedian * recYdsMultiplier
+projections$recTdsMedianPts <- projections$recTdsMedian * recTdsMultiplier
+projections$twoPtsMedianPts <- projections$twoPtsMedian * twoPtsMultiplier
+projections$fumblesMedianPts <- projections$fumblesMedian * fumlMultiplier
+
+projections$projectedPtsMean <- rowSums(projections[,c("passYdsPts","passTdsPts","passIntPts","rushYdsPts","rushTdsPts","recPts","recYdsPts","recTdsPts","twoPtsPts","fumblesPts")], na.rm=T)
+projections$projectedPtsMedian <- rowSums(projections[,c("passYdsMedianPts","passTdsMedianPts","passIntMedianPts","rushYdsMedianPts","rushTdsMedianPts","recMedianPts","recYdsMedianPts","recTdsMedianPts","twoPtsMedianPts","fumblesMedianPts")], na.rm=T)
 
 #Calculate latent variable for projected points
-cor(projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"),"projectedPtsAvg")], use="pairwise.complete.obs")
+cor(projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"), c("projectedPtsMean","projectedPtsMedian"))], use="pairwise.complete.obs")
 
 factor.analysis <- factanal(projections[,paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_")], factors = 1, scores = "Bartlett") #factor.analysis <- factanal(~projectedPts_espn + projectedPts_cbs + projectedPts_nfl + projectedPts_fs + projectedPts_fp, factors = 1, scores = "Bartlett", data=projections) #regression
 
@@ -129,23 +159,23 @@ factor.loadings
 projectedPtsLatent <- as.vector(factor.scores)
 
 #Best fitting distribution of Average Fantasy Points
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 't')$loglik
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'normal')$loglik
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'logistic')$loglik
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'weibull')$loglik #best
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'gamma')$loglik
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'lognormal')$loglik
-fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'exponential')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 't')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'normal')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'logistic')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'weibull')$loglik #best
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'gamma')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'lognormal')$loglik
+fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'exponential')$loglik
 
-weibullShape <- fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'weibull')$estimate[[1]]
-weibullScale <- fitdistr(projections$projectedPtsAvg - floor(min(projections$projectedPtsAvg, na.rm=TRUE)), 'weibull')$estimate[[2]]
+weibullShape <- fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'weibull')$estimate[[1]]
+weibullScale <- fitdistr(projections$projectedPtsMedian - floor(min(projections$projectedPtsMedian, na.rm=TRUE)), 'weibull')$estimate[[2]]
 
 projectedPtsLatentWeibull <- qweibull(pnorm(projectedPtsLatent), shape=weibullShape, scale=weibullScale)
-projectedPtsLatentWeibullRescaled <- rescaleRange(variable=projectedPtsLatentWeibull, minOutput=0, maxOutput=max(projections$projectedPtsAvg))
+projectedPtsLatentWeibullRescaled <- rescaleRange(variable=projectedPtsLatentWeibull, minOutput=0, maxOutput=max(projections$projectedPtsMedian))
 
 projections$projectedPtsLatent <- projectedPtsLatentWeibullRescaled
 
-projectionVars <- projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"), "projectedPtsAvg", "projectedPtsLatent")]
+projectionVars <- projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"), c("projectedPtsMean","projectedPtsMedian","projectedPtsLatent"))]
 
 #Convert Zeros to NA
 for(i in 1:length(sourcesOfProjectionsAbbreviation)){
@@ -156,11 +186,12 @@ is.na(projectionVars) <- projectionVars==0
 
 #Describe
 describe(projectionVars)
-plot(density(na.omit(projections$projectedPtsAvg)))
-lines(density(na.omit(projections$projectedPtsLatent)))
+plot(density(na.omit(projections$projectedPtsMean)), col="black")
+lines(density(na.omit(projections$projectedPtsMedian)), col="blue")
+lines(density(na.omit(projections$projectedPtsLatent)), col="red")
 
 #Correlations among projections
-cor(projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"),"projectedPtsAvg","projectedPtsLatent")], use="pairwise.complete.obs")
+cor(projections[,c(paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"), c("projectedPtsMean","projectedPtsMedian","projectedPtsLatent"))], use="pairwise.complete.obs")
 
 #Set criterion for projections based on whose projections are most accurate
 projections$projections <- projections$projectedPts_fp
@@ -175,14 +206,14 @@ projections <- projections[order(projections$overallRank),]
 row.names(projections) <- 1:dim(projections)[1]
 
 #Keep important variables
-projections <- projections[,c("name","player","pos","team","overallRank","projections",paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"),"projectedPtsAvg","projectedPtsLatent")]
+projections <- projections[,c("name","player","pos","team","overallRank","projections",paste("projectedPts", sourcesOfProjectionsAbbreviation, sep="_"), c("projectedPtsMean","projectedPtsMedian","projectedPtsLatent"))]
 
 #View projections
 projections
-projections[,c("name","pos","team","projectedPts_fp","projectedPtsAvg","projectedPtsLatent")]
+projections[,c("name","pos","team","projectedPts_fp","projectedPtsMean","projectedPtsMedian","projectedPtsLatent")]
 
 #Density Plot
-pointDensity <- c(projections$projectedPts_accu, projections$projectedPts_espn, projections$projectedPts_cbs, projections$projectedPts_nfl, projections$projectedPts_fs, projections$projectedPts_fp, projections$projectedPts_yahoo, projections$projectedPtsAvg) #,projections$projectedPtsLatent
+pointDensity <- c(projections$projectedPts_accu, projections$projectedPts_espn, projections$projectedPts_cbs, projections$projectedPts_nfl, projections$projectedPts_fs, projections$projectedPts_fp, projections$projectedPts_yahoo, projections$projectedPtsMean) #,projections$projectedPtsLatent
 sourceDensity <- c(rep("Accuscore",dim(projections)[1]), rep("ESPN",dim(projections)[1]), rep("CBS",dim(projections)[1]), rep("NFL",dim(projections)[1]), rep("FS",dim(projections)[1]), rep("FP",dim(projections)[1]), rep("Yahoo",dim(projections)[1]), rep("Average",dim(projections)[1])) #,rep("Latent",dim(projections)[1])
 densityData <- data.frame(pointDensity, sourceDensity)
 
