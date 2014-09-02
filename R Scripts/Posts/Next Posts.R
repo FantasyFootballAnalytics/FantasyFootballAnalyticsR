e6a1770886dd143b928446b89e995c1ab7d235c6
Isaac Petersen <dadrivr@gmail.com>
2014-09-01 22:59:14 -0400

    20140901 - update to-do list

--- R Scripts/Posts/Next Posts.R
+++ R Scripts/Posts/Next Posts.R
@@ -6,7 +6,8 @@
 ###########
 # Apps
 ###########
-# 1. Weekly projections
+# 1. Add PPR ADP: http://fantasyfootballcalculator.com/adp_csv.php?format=ppr&teams=12
+# 2. Weekly projections
 
 ###########
 # Other sources of projections
