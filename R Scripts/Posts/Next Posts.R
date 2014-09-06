6c3f90e2ad696a38a4d85d60f2644306ad7689e3
Isaac Petersen <dadrivr@gmail.com>
2014-09-06 13:22:45 -0400

    20140906 - next posts

--- R Scripts/Posts/Next Posts.R
+++ R Scripts/Posts/Next Posts.R
@@ -4,6 +4,8 @@
 # 1. Re-evaluate projections
 # 2. Compare accuracy of projections and rankings in predicting final rankings
 # 3. Compare accuracy of different values for "typical replacement" players (average starter, worst starter, players drafted within first 100 picks, “man games”: http://fantasyfootballanalytics.net/2013/09/win-your-fantasy-football-snake-draft.html#comment-11470): criterion: percentage of playoff teams a certain player is on
+# 4. How to Run R Scripts
+# 5. How to use FFA GitHub repo
 
 ###########
 # Apps
