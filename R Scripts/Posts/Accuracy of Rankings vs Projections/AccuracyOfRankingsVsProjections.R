8b40f71a6826453c275d23930ddf918079adbd15
Isaac Petersen <isaac@isaacs-mbp.attlocal.net>
2016-06-05 09:56:56 -0400

    20160605 - accuracy of adp vs projections

--- R Scripts/Posts/Accuracy of Rankings vs Projections/AccuracyOfRankingsVsProjections.R
+++ R Scripts/Posts/Accuracy of Rankings vs Projections/AccuracyOfRankingsVsProjections.R
@@ -21,11 +21,12 @@ projections2015 <- data.table(projections2015)
 
 #Determine within-position preseason rankings
 projections2015[is.finite(overallECR), preseasonRankings := rank(overallECR, ties.method = "min"), by = position]
+projections2015[is.finite(adp), preseasonADP := rank(adp, ties.method = "min"), by = position]
 
 #Subset accuracy data (compare only the same players by removing missing data)
 projections2015[, preseasonProjections := points]
 
-accuracy2015 <- projections2015[, c("playername","position","preseasonProjections","preseasonRankings","overallECR","actualPoints"), with=FALSE]
+accuracy2015 <- projections2015[, c("playername","position","preseasonProjections","preseasonRankings","overallECR","preseasonADP","adp","actualPoints"), with=FALSE]
 accuracy2015 <- accuracy2015[complete.cases(accuracy2015),]
 
 setkey(accuracy2015, playername)
@@ -38,6 +39,7 @@ accuracy2015
 #Calculate Accuracy of Projections vs. Rankings
 summary(lm(actualPoints ~ preseasonProjections, data=accuracy2015))$r.squared
 summary(lm(actualPoints ~ overallECR, data=accuracy2015))$r.squared
+summary(lm(actualPoints ~ adp, data=accuracy2015))$r.squared
 
 #Calculate Accuracy of Projections vs. Rankings by Position
 summary(lm(actualPoints ~ preseasonProjections, data=accuracy2015[position == "QB",]))$r.squared
@@ -53,3 +55,10 @@ summary(lm(actualPoints ~ preseasonRankings, data=accuracy2015[position == "WR",
 summary(lm(actualPoints ~ preseasonRankings, data=accuracy2015[position == "TE",]))$r.squared
 summary(lm(actualPoints ~ preseasonRankings, data=accuracy2015[position == "K",]))$r.squared
 summary(lm(actualPoints ~ preseasonRankings, data=accuracy2015[position == "DST",]))$r.squared
+
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "QB",]))$r.squared
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "RB",]))$r.squared
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "WR",]))$r.squared
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "TE",]))$r.squared
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "K",]))$r.squared
+summary(lm(actualPoints ~ preseasonADP, data=accuracy2015[position == "DST",]))$r.squared
