e2a12a9192d2707bd546b2314257878c20fc89e3
Dennis Andersen <andersen.dennis@live.com>
2016-06-13 16:22:29 -0500

    Update retrieveData.R

--- R Package/ffanalytics/R/retrieveData.R
+++ R Package/ffanalytics/R/retrieveData.R
@@ -218,7 +218,11 @@ retrieveData <- function(srcTbl, srcPeriod, fbgUser = NULL, fbgPwd = NULL){
 
   if(idVar != "playerId" & nrow(dataTable) > 0){# & exists(ifelse(nchar(idVar) > 0, idVar, "_none_"), dataTable)){
     playerIds <- intersect(names(playerData), c("playerId", "player", "cbsId", "mflId", "yahooId", "fbgId"))
-    idTbl <- playerData[position == srcTbl@sourcePosition, playerIds, with = FALSE]
+    if(srcTbl@sourcePosition %in% c("DL", "LB", "DB)){
+      idTbl <- playerData[position  %in% c("DL", "LB", "DB), playerIds, with = FALSE]
+    } else {
+      idTbl <- playerData[position == srcTbl@sourcePosition, playerIds, with = FALSE]
+    }
 
     if(length(idVar) > 0 & nchar(idVar) > 0 & srcTbl@sourcePosition != "DST"){
       if(!(idVar %in% names(idTbl))){
