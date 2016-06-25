a32d50a03e2b496744bff5650ec26844edcc8b72
Dennis Andersen <andersen.dennis@live.com>
2016-06-24 19:52:54 -0500

    Updating to get better player matches on names and teams

--- R Package/ffanalytics/R/internalConstants.R
+++ R Package/ffanalytics/R/internalConstants.R
@@ -6,15 +6,17 @@ urlParameters <-  c("{$WeekNo}", "{$Season}", "{$Segment}",  "{$PgeID}", "{$Yaho
 sharkSegment = c("2015" = 522, "2016" = 554)
 
 websites <- c("footballguys", "fftoday", "fantasysharks", "walterfootball", "yahoo", "fox", "numberfire", "rotowire", "rtsports", "fantasyfootballnerd",
-              "cbssports", "4for4", "espn", "nfl.com", "fantasypros", "eatdrinkandsleepfootball")
+              "cbssports", "4for4", "espn", "nfl.com", "fantasypros", "eatdrinkandsleepfootball", "fantasydata")
 
 correct.from <- c("Ty Hilton", "Timothy Wright", "Christopher Ivory", "Domanique Davis", "Ben Watson", "Stevie Johnson",	"Lesean McCoy",
                   "Luke Wilson", "Thaddeus Lewis", "Walter Powell",  "Wilson VanHooser", "Steve Hauschka", "Daniel Herron", "Robert Housler" ,
                   "Corey Philly Brown",  "Foswhitt Whittaker", "CJ Anderson", "TY Hilton", "Boobie Dixon", "EZ Nwachukwu", "Dave Paulson",
-                  "Joe DonDuncan", "T Y Hilton", "Dqwell Jackson", "Art Jones", "Navorro Bowman", "Devante Parker")
+                  "Joe DonDuncan", "T Y Hilton", "Dqwell Jackson", "Art Jones", "Navorro Bowman", "Devante Parker", "A.J. McCarron", "TJ Yeldon",
+                  "CJ Prosise", "AJ Green")
 
 correct.to <- c("T.Y. Hilton", "Tim Wright", "Chris Ivory", "Dominique Davis", "Benjamin Watson", "Steve Johnson", "LeSean McCoy",
                 "Luke Willson",	"Thad Lewis", "Walt Powell", "Wilson Van Hooser", "Steven Hauschka", "Dan Herron", "Rob Housler",
                 "Philly Brown", "Fozzy Whittaker", "C.J. Anderson", "T.Y. Hilton", 	"Anthony Dixon", "Uzoma Nwachukwu", "David Paulson",
-                "Joe Don Duncan",	"T.Y. Hilton", "DQwell Jackson", "Arthur Jones", "NaVorro Bowman", "DeVante Parker")
+                "Joe Don Duncan",	"T.Y. Hilton", "DQwell Jackson", "Arthur Jones", "NaVorro Bowman", "DeVante Parker", "AJ McCarron",
+                "T.J. Yeldon", "C.J. Prosise", "A.J. Green")
 
