79988f8b016ee44cf518db27872a240512e4379d
Dennis Andersen <andersen.dennis@live.com>
2016-06-25 00:01:16 -0500

    Adding team name function

---
+++ R Package/ffanalytics/R/extractTeamNames.R
@@ -0,0 +1,37 @@
+#' @export extractTeam
+extractTeam <- function(teamInfo, siteName){
+  teams <- vector(mode = "character", length = length(teamInfo))
+  nfl.teams <- c(gsub("WAS", "WSH", gsub("JAC", "JAX", nflTeam.abb)), "FA")
+  switch (siteName,
+    "fantasypros" = {
+      fd.pattern <- paste(paste0(nfl.teams, "$") , collapse = "|")
+      teams <- stringr::str_extract(teamInfo, fd.pattern)
+    },
+
+    "espn" = {
+      espn.pattern <- paste(paste(",", nfl.teams), collapse = "|")
+      teams <- stringr::str_extract(toupper(teamInfo), espn.pattern)
+      teams <- gsub(", ", "", teams, fixed = TRUE)
+    },
+
+    "numberfire" = {
+      nfm.pattern <- paste(paste(",", nfl.teams), collapse = "|")
+      teams <- stringr::str_extract(teamInfo, nfm.pattern)
+      teams <- gsub(", ", "", teams, fixed = TRUE)
+    },
+
+    "fox" = {
+      tm.info <- stringr::str_extract(teamInfo, "[:alpha:]+ - [:alpha:]+")
+      fox.pattern <- paste(nfl.teams, collapse = "|")
+      teams <- stringr::str_extract(toupper(tm.info), fox.pattern)
+    },
+
+    "footballguys" = {
+      fbg.pattern <- paste(nfl.teams, collapse = "|")
+      teams <- stringr::str_extract(teamInfo, fbg.pattern)
+    }
+
+  )
+
+  return(teams)
+}
